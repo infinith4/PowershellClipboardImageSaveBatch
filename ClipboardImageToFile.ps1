e476fbde06af0389f27ca65af28c29ca7d513374
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-07-17 12:49:11 +0900

    jpeg format (bad quality)

--- ClipboardImageToFile.ps1
+++ ClipboardImageToFile.ps1
@@ -1,16 +1,22 @@
 
-Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Windows.Forms, System.Drawing
+
 $clipboardImage = [Windows.Forms.Clipboard]::GetImage()
 if ($clipboardImage -ne $null)
 {
   $dateTimeStr = Get-Date -Format "yyyyMMdd_HHmmss"
-  $fileName = $dateTimeStr + "_clip.png"
-  Write-Output $fileName
+  $fileName = $dateTimeStr + "_clip.jpg"
+  #Write-Output $fileName
   $picturesDirPath = Join-Path $Env:UserProfile "Pictures"
   $clipboardImagesDirPath = Join-Path $picturesDirPath "ClipboardImages"
-  Write-Output $clipboardImagesDirPath
+  #Write-Output $clipboardImagesDirPath
   New-Item -ItemType Directory -Force -Path $clipboardImagesDirPath
   $outputFilePath = Join-Path $clipboardImagesDirPath $fileName
-  Write-Output $outputFilePath
-  $clipboardImage.Save($outputFilePath)
+  #Write-Output $outputFilePath
+  #save as jpeg format
+  # #保存形式と保存画質の初期設定
+  # $encoderQuality = [System.Drawing.Imaging.Encoder]::Quality
+  # $encoderParams = New-Object System.Drawing.Imaging.EncoderParameters(1)
+  # $encoderParams.Param[0] = New-Object System.Drawing.Imaging.EncoderParameter($myEncoder, $quality)
+  $clipboardImage.Save($outputFilePath, [System.Drawing.Imaging.ImageFormat]::Jpeg)
 }
